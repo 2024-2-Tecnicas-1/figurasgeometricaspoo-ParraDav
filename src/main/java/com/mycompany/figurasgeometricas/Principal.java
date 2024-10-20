dd785e9cc821ae3cfcb7d8a423f928d469b4d23b
ParraDav <df.parra.07@gmail.com>
2024-10-20 13:17:04 -0500

    Primeros cambios en el main
    Se añade código en el main, la primera parte de el menú necesario para la ejecución del código, con posibles cambios en el futuro

--- src/main/java/com/mycompany/figurasgeometricas/Principal.java
+++ src/main/java/com/mycompany/figurasgeometricas/Principal.java
@@ -1,8 +1,35 @@
 package com.mycompany.figurasgeometricas;
 
+import java.util.Scanner;
+
 public class Principal {
 
     public static void main(String[] args) {
-        // TODO: Tu código va aquí
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ingrese el nombre de la figura");
+        String name = sc.nextLine();
+        System.out.println("Ingrese el color de la figura");
+        String color = sc.nextLine();
+        System.out.println("Ingrese el tipo de figura");
+        char tipo = ' ';
+        do {
+            System.out.println("1. Circulo");
+            System.out.println("2. Rectángulo");
+            System.out.println("3. Triángulo");
+            tipo = sc.next().charAt(0);
+            tipo = Character.toLowerCase(tipo);
+            switch (tipo) {
+                case '1':
+                    break;
+                case '2':
+                    break;
+                case '3':
+                    break;
+                default:
+                    throw new AssertionError();
+            }
+        } while (tipo != '0');
+
     }
 }
